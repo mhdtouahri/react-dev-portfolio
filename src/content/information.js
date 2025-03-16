d5e7fc8c308cd75addecb4626aa3c5e8a504e208
mhdtrh <mohandtouahri2@gmail.com>
2025-03-16 15:51:41 +0100

    add name

--- src/content/information.js
+++ src/content/information.js
@@ -1,7 +1,7 @@
 //personal information start
 
 const userData = {
-  firstName: "<Randil",
+  firstName: "<Mohand",
   lastName: "Tharusha/>",
   title: "About Me",
   img: "https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp",
